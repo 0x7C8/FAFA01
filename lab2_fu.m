c105257a494fd1a256059c6575f6c5d57990dbf9
Georgij <georgij92@gmail.com>
2019-04-08 20:31:00 +0200

    lab 2 förberedelse - done

--- lab2_fu.m
+++ lab2_fu.m
@@ -11,7 +11,7 @@ labelSettings = {...
     'Interpreter','latex',...
     'Fontsize', 18};
 
-lineSpec = {'k-','k--','k:', 'k-.'};
+lineSpec = {'b-','r--','k:', 'k-.'};
 % -------------------------------------
 
 %% 1a
@@ -36,17 +36,29 @@ xticks(0:.5:2)
 v = diff(y)./diff(t);
 
 figure(2)
-plot(t(1:length(v)),v)
+plot(y(1:length(v)),v)
 set(gca,gcaSettings(1:2:end),gcaSettings(2:2:end))
-xlabel('$t/s$',labelSettings(1:2:end), labelSettings(2:2:end))
+xlabel('$y/m$',labelSettings(1:2:end), labelSettings(2:2:end))
 ylabel('$v/ms^{-1}$',labelSettings(1:2:end), labelSettings(2:2:end))
-xticks(0:.5:2)
+%xticks(0:.5:2)
 
 %% 2a
 b = 0.4; % kg/s
 
 y_half = 2*m*log(2)/b;
-answer = ["2a: "+num2str(y_half,'%.3f') + " s"]
+answers = ["2a: "+num2str(y_half,'%.3f') + " s"]
+
+damp = b/(2*m);
+if damp > w
+    dr = 'Overdamped';
+elseif damp == 0
+    dr = 'Undamped';
+elseif damp == w
+    dr = 'Critically damped';
+else
+    dr = 'Underdamped';
+end
+fprintf('The spring is %s\n', dr)
 
 %% 2b
 w_d = sqrt(w^2-(b/(2*m))^2);
@@ -59,7 +71,30 @@ plot(t,-y_exp,lineSpec{2})
 plot(t,y_damp,lineSpec{1})
 
 %% 2c
+v_damp = diff(y_damp)./diff(t);
 
-%% 3
+figure(3)
+plot(y_damp(1:length(v)),v_damp)
+set(gca,gcaSettings(1:2:end),gcaSettings(2:2:end))
+xlabel('$y/m$',labelSettings(1:2:end), labelSettings(2:2:end))
+ylabel('$v/ms^{-1}$',labelSettings(1:2:end), labelSettings(2:2:end))
+
 
 %% 4
+L = 12*10^(-3); %H
+C = 1.6*10^(-6); %F
+f = 1000; %Hz
+
+w_RCL = 2*pi*f;
+R = 2*L*sqrt(1/(L*C)-(w_RCL)^2);
+t_half = 2*log(2)*L/R;
+answers = ["4a: "+num2str(R,'%.3f') + " Ohm" + newline ...
+     + "4b: " + num2str(t_half*1000,'%.3f') + " ms"]
+
+
+
+
+
+
+
+
