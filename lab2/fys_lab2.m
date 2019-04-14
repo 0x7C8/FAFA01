009d73d34d2d2cc5a76d40ea821b9ce5dfd28e1b
Georgij <georgij92@gmail.com>
2019-04-14 23:24:41 +0200

    more work done on lab 2

--- lab2/fys_lab2.m
+++ lab2/fys_lab2.m
@@ -50,10 +50,12 @@ w_1 = 2*pi * numel(pks)/ (t_1(i_1 + loc(end)) - t_1(i_1 + loc(1)));
 % w_1 = 3.867;  % Counted manually =O
 
 figure(1)
-plot(t_1,y_1)
+plot(t_1,y_1,'LineWidth',2)
 set(gca,gcaSettings(1:2:end),gcaSettings(2:2:end))
-xlabel(col(2),labelSettings(1:2:end), labelSettings(2:2:end))
-ylabel(col(3),labelSettings(1:2:end), labelSettings(2:2:end))
+xlabel('$t/s$',labelSettings(1:2:end), labelSettings(2:2:end))
+ylabel('$y/m$',labelSettings(1:2:end), labelSettings(2:2:end))
+axis([0 30 -0.3 0.3])
+saveas(gcf,'fig1_1','epsc')
 
 % Finding velocity
 dy = diff(y_1);
@@ -63,8 +65,12 @@ v = dy./dt;
 figure(2)
 plot(y_1(2:end),v)
 set(gca,gcaSettings(1:2:end),gcaSettings(2:2:end))
-xlabel(col(3),labelSettings(1:2:end), labelSettings(2:2:end))
-ylabel(col(4),labelSettings(1:2:end), labelSettings(2:2:end))
+xlabel('$y/m$',labelSettings(1:2:end), labelSettings(2:2:end))
+ylabel('$v/ms^{-1}$',labelSettings(1:2:end), labelSettings(2:2:end))
+axis(1.1*[min(y_1) max(y_1) min(v) max(v)])
+xticks(-1:.1:1)
+yticks(-1:.5:1)
+saveas(gcf,'fig1_2','epsc')
 
 % Energy
 K = v.^2;
@@ -77,9 +83,28 @@ plot(t_1(2:end),K,lineSpec{2})
 plot(t_1,U,lineSpec{1})
 plot(t_1(2:end),E,lineSpec{4})
 set(gca,gcaSettings(1:2:end),gcaSettings(2:2:end))
-xlabel(col(2),labelSettings(1:2:end), labelSettings(2:2:end))
-ylabel('Energy (ummm)',labelSettings(1:2:end), labelSettings(2:2:end))
+xlabel('$t/s$',labelSettings(1:2:end), labelSettings(2:2:end))
+ylabel('$E/m^{2}s^{-2}$',labelSettings(1:2:end), labelSettings(2:2:end))
 axis([0 15 0 1.5])
+
+% Finding half life
+
+% figure(31)
+% findpeaks(E(10:400), 'MinPeakDistance',10)
+% axis([0 300 0 max(E)])
+[E_pk, E_loc] = findpeaks(E(10:400), 'MinPeakDistance',10);
+% plot(E_loc, E_pk)
+pk_select = 10;
+t_half = log(E_pk(1)/E_pk(pk_select))/(t_1(E_loc(pk_select)+1)-t_1(E_loc(1)+1));
+Halveringstid = "Figur 3: " + 1/t_half + " s"
+
+chill = @(t) E_pk(1).*exp(-(t-t_1(E_loc(1))).*t_half);
+plot(t_1+t_1(E_loc(1)), chill(t_1),'r--', 'LineWidth',2)
+
+legend('K','V','E','Halveringstid');
+xticks(0:2:20)
+yticks(0:.5:2)
+saveas(gcf,'fig1_3','epsc')
 
 %% Part 2
 
@@ -111,21 +136,41 @@ y_2m = y_2m - mean(y_2m(8000:9000));
 % Testing normalisation
 % visualmeananalysis(y_2m,100)
 
-% Finding angular velocity through finding peaks
+% Finding angular velocity for max amplitude through finding peaks
 [mx, pos] = max(y_2s);
-[pks, loc] = findpeaks(y_2s(pos-100:pos+200));
-w_2 = 2*pi * numel(pks)/ (t_2(pos+200) - t_2(pos-100));
+[s_pks, s_loc] = findpeaks(y_2s(pos-100:pos+100));
+w_2s_max = 2*pi * numel(s_pks)/ (t_2(s_loc(end)) - t_2(s_loc(1)));
+
+% Finding angular velocity for motor through finding peaks
+[m_pks, m_loc] = findpeaks(y_2m(pos-100:pos+100));
+w_2m = 2*pi * numel(m_pks)/ (t_2(m_loc(end)) - t_2(m_loc(1)));
 
 % Finding angle between 2 curves
+i = -100:5:100;
+index = pos+i;
+deg_s = mod((t_2(index) - t_2(s_loc(1)))*w_2s_max*180/pi,360);
+deg_m = mod((t_2(index) - t_2(m_loc(1)))*w_2m*180/pi,360);
 
+for i=1:length(deg_s)
+    alpha(i) = deg_s(i) - deg_m(i);
+    if deg_s(i) < deg_m(i)
+        alpha(i) = alpha(i) + 360;
+    end
+end
+figure(4)
+hold on
+plot(t_2(index), sind(alpha))
+ax = axis;
+plot([t_2(pos), t_2(pos)],[-1.5 1.5],'k-')
+axis(ax)
 
 
 %% Part 3
 L = 9*10^(-3); %H
 C = 0.471*10^(-6); %F
+R_L = 3; %Ohm Measured with multimeter
+R = 0;  
 %f = 1000; %Hz
-R_L = 2.5; %Ohm
-R = 0;
 
 %w_RCL = 2*pi*f;
 w_0 = sqrt(1/(L*C));
